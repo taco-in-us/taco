64ed922b3d744604194d547fc3161ca6a68dcb03
kylie-dev <kylie.dev@kakaocorp.com>
2019-03-10 17:16:19 +0900

    스프링부트 2.1.3 세팅

---
+++ src/main/java/com/tacoinus/taco/TacoApplication.java
@@ -0,0 +1,13 @@
+package com.tacoinus.taco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TacoApplication.class, args);
+  }
+
+}
